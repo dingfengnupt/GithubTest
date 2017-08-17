5c25da946511dbed583106715bc1f1915d1e1c62
dingfeng <dingfeng@hujiang.com>
2017-08-17 10:47:11 +0800

    modify

---
+++ test_library/src/main/java/com/githubtest/library/Test.java
@@ -0,0 +1,15 @@
+package com.githubtest.library;
+
+import android.util.Log;
+
+/**
+ * Created by dingfeng on 2017/8/17.
+ */
+
+public class Test {
+
+    public static void printLog() {
+        Log.d("dingfeng", "This is just test !");
+    }
+
+}
